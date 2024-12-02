b02bbf977c30bca4b27e862a7ca4a756a0e49972
LUIGIWONKA <mejiaguetoestiven@gmail.com>
2024-12-02 16:00:30 -0500

    Fixed lint errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,41 +1,46 @@
-import globals from "globals";
-import js from "@eslint/js";
-import stylisticJs from "@stylistic/eslint-plugin-js";
+import globals from 'globals'
+import js from '@eslint/js'
+import stylisticJs from '@stylistic/eslint-plugin-js'
 
 export default [
   {
-    ignores: ["**/dist/**", "**/cypress/**", "**/public/**"],
+    ignores: ['**/dist/**', '**/cypress/**', '**/public/**', '**/*.js'],
   },
   js.configs.recommended,
   {
-    files: ["**/*.js"],
+    files: ['**/*.js'],
     languageOptions: {
-      sourceType: "module",
+      sourceType: 'module',
       globals: {
         ...globals.node,
       },
-      ecmaVersion: "latest",
+      ecmaVersion: 'latest',
     },
     plugins: {
-      "@stylistic/js": stylisticJs,
+      '@stylistic/js': stylisticJs,
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
     },
     rules: {
-      "parser": '@babel/eslint-parser',
-      "@stylistic/js/indent": ["error", 2],
-      "@stylistic/js/linebreak-style": ["error", "unix"],
-      "@stylistic/js/quotes": ["error", "single"],
-      "@stylistic/js/semi": ["error", "never"],
-      indent: ["error", 2],
-      "linebreak-style": ["error", "unix"],
-      quotes: ["error", "single"],
-      semi: ["error", "never"],
-      eqeqeq: "error",
-      "no-trailing-spaces": "error",
-      "object-curly-spacing": ["error", "always"],
-      "arrow-spacing": ["error", { before: true, after: true }],
-      "no-console": 0,
-      "react/react-in-jsx-scope": "off",
-      "no-unused-vars": 0,
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/quotes': ['error', 'single'],
+      '@stylistic/js/semi': ['error', 'never'],
+      indent: ['error', 2],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'never'],
+      eqeqeq: 'error',
+      'no-trailing-spaces': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'no-console': 0,
+      'no-unused-vars': 0,
+      'react/react-in-jsx-scope': 0,
+      'react/prop-types': 0,
     },
   },
-];
+]
